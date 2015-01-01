926273538a2f3b4c81087f5777ae5866034ff3d1
rmbishop <bmk7800@gmail.com>
2015-01-01 14:08:31 -0600

    Update test2.lua

--- test2.lua
+++ test2.lua
@@ -166,32 +166,11 @@ myvar = [[
         <!-- Sidebar -->
         <div id="sidebar-wrapper">
             <ul class="sidebar-nav">
-                <li class="sidebar-brand">
-                    <a href="#">
-                        Start Bootstrap
-                    </a>
-                </li>
+                {{#slug_list}}
                 <li>
-                    <a href="#">Dashboard</a>
+                    <a href="#">{{slug_name}}</a>
                 </li>
-                <li>
-                    <a href="#">Shortcuts</a>
-                </li>
-                <li>
-                    <a href="#">Overview</a>
-                </li>
-                <li>
-                    <a href="#">Events</a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
+                {{/slugs}}
             </ul>
         </div>
         <!-- /#sidebar-wrapper -->
